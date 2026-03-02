314692f8123f0f7bb5bdc82c76ffa60912dda4f8
bryanltran <bryan.l.tran-1@ou.edu>
2026-03-02 12:46:58 -0600

    Update Launch and World

--- src/project1/launch/launch.py
+++ src/project1/launch/launch.py
@@ -1,33 +1,32 @@
-import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    world_file = os.path.join(
-        get_package_share_directory('project1'),
-        'worlds',
-        'world.sdf'
-    )
+    pkg_share = get_package_share_directory('project1')
+    default_world = os.path.join(pkg_share, 'worlds', 'world.sdf')
 
-    tb4_launch = os.path.join(
-        get_package_share_directory('turtlebot4_gz_bringup'),
-        'launch',
-        'turtlebot4_gz.launch.py'
-    )
+    world = LaunchConfiguration('world')
+    slam = LaunchConfiguration('slam')
+    headless = LaunchConfiguration('headless')
+
+    nav2_share = get_package_share_directory('nav2_bringup')
+    tb4_sim_launch = os.path.join(nav2_share, 'launch', 'tb4_simulation_launch.py')
 
     return LaunchDescription([
+        DeclareLaunchArgument('world', default_value=default_world),
+        DeclareLaunchArgument('slam', default_value='True'),
+        DeclareLaunchArgument('headless', default_value='False'),
+
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(tb4_launch),
+            PythonLaunchDescriptionSource(tb4_sim_launch),
             launch_arguments={
-                'gazebo': 'ignition',
-                'gz_args': world_file,
-                'x': '1.0',
-                'y': '2.0',
-                'z': '0.0',
-                'yaw': '0.0',
-                'model': 'standard',
+                'world': world,
+                'slam': slam,
+                'headless': headless,
             }.items()
         ),
     ])
